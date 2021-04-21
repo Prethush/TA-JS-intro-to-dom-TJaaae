5f84a3271bad69ca81924f87f9e999d63c24d979
prethush <ktmirash49@gmail.com>
2021-04-21 14:03:30 +0530

    DOM

--- block-BJaaeb/code/script.js
+++ block-BJaaeb/code/script.js
@@ -6,6 +6,13 @@ default value to be "text" and return the input element inside label. (create it
 */
 
 // Your code goes here
+function createInputElm(label, type = "text") {
+  let label1 = document.createElement("label");
+  let input = document.createElement("input");
+  input.type = type;
+  label1.append(label, input);
+  return label1;
+}
 
 // TEST
 createInputElm('Your name'); //<label>Your name: <input type="text"></label>
@@ -23,9 +30,20 @@ createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></
 // the html for the link like <ul> <li>Mango</li>  <li>Apple</li>  <li>Banana</li> </ul>
 // Your code goes here
 
+function createList(arr) {
+  let ul = document.createElement("ul");
+  let li = [];
+  for(let i = 0; i < arr.length; i++) {
+     li[i] = document.createElement("li");
+    li[i].append(arr[i]);
+  }
+  li.forEach((el) => ul.append(el));
+  return ul;
+}
 // TEST
 createList(['ALABAMA', 'ALASKA', 'HAWAII', 'KENTUCKY']);
 createList(['Afghanistan', 'Antarctica', 'Congo', 'Estonia']);
+
 
 // 4. Create a function named `createTodoList` that accept and array of data like [{name: "Learn DOM", isDone: false}, {name: "Learn JS", isDone: true}] and returns
 // the html for single todo will look like given below
@@ -41,6 +59,28 @@ createList(['Afghanistan', 'Antarctica', 'Congo', 'Estonia']);
 
 // Your code goes here
 
+function createTodoList(arr) {
+  let ul =[], p = [], li = [], input = [], span = [];
+  for(let i = 0; i < arr.length; i++) {
+    ul[i] = document.createElement("ul");
+    li[i] = document.createElement("li");
+    p[i] = document.createElement("p");
+    input[i] = document.createElement("input");
+    span[i] = document.createElement("span");
+    p[i].innerText = arr[i].name;
+    span[i].innerText = "X";
+    input[i].type = "checkbox";
+    input[i].name = "";
+    input[i].id = "";
+    input[i].checked = arr[i].isDone;
+    li[i].append(p[i], input[i], span[i]);
+    ul[i].append(li[i]);
+  } 
+ 
+ for(let i = 0;  i < ul.length; i++) {
+   console.log(ul[i]);
+ }
+}
 // TEST
 createTodoList([
   { name: 'Learn DOM', isDone: false },
